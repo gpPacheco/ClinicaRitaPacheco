106e4808796a4d028912800f94dd093d94e0f10d
Gabriel Pacheco <gabrielpacheco@MacBook-Pro-de-Gabriel.local>
2024-06-25 10:05:40 -0300

    ajustes footer

--- src/app/footer/index.tsx
+++ src/app/footer/index.tsx
@@ -1,6 +1,14 @@
 "use client";
 import Image from "next/image";
-import { FaWhatsapp, FaInstagram, FaFacebook, FaLinkedin, FaTiktok, FaRegAddressBook } from "react-icons/fa";
+import {
+  FaWhatsapp,
+  FaInstagram,
+  FaFacebook,
+  FaLinkedin,
+  FaTiktok,
+  FaRegAddressBook,
+} from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
 
 export function Rodape() {
   return (
@@ -27,29 +35,45 @@ export function Rodape() {
                 <span className="sr-only"> WhatsApp </span>
                 <FaWhatsapp className="w-6 h-6" />
               </a>
-              <a className="hover:opacity-75" target="_blank" rel="noreferrer" 
-              href="https://www.instagram.com/ritafpacheco/">
+              <a
+                className="hover:opacity-75"
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.instagram.com/ritafpacheco/"
+              >
                 <span className="sr-only"> Instagram </span>
                 <FaInstagram className="w-6 h-6" />
               </a>
-              <a className="hover:opacity-75" target="_blank" rel="noreferrer"
-              href="https://www.facebook.com/ritapachecopodologa">
+              <a
+                className="hover:opacity-75"
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.facebook.com/ritapachecopodologa"
+              >
                 <span className="sr-only"> Facebook </span>
                 <FaFacebook className="w-6 h-6" />
               </a>
-              <a className="hover:opacity-75" target="_blank" rel="noreferrer"
-              href="https://www.linkedin.com/company/clinica-rita-pacheco/">
+              <a
+                className="hover:opacity-75"
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.linkedin.com/company/clinica-rita-pacheco/"
+              >
                 <span className="sr-only"> LinkedIn </span>
                 <FaLinkedin className="w-6 h-6" />
               </a>
-              <a className="hover:opacity-75" target="_blank" rel="noreferrer"
-              href="https://www.tiktok.com/@ritafpachecoo">
+              <a
+                className="hover:opacity-75"
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.tiktok.com/@ritafpachecoo"
+              >
                 <span className="sr-only"> TikTok </span>
                 <FaTiktok className="w-6 h-6" />
               </a>
             </div>
           </div>
-          <div className="grid grid-cols-1 gap-8 lg:col-span-2 sm:grid-cols-2 lg:grid-cols-4">
+          <div className="grid grid-cols-1 gap-3 lg:col-span-2 sm:grid-cols-2 lg:grid-cols-4">
             <div>
               <p className="font-medium">Links Rápidos</p>
               <nav className="flex flex-col mt-4 space-y-2 text-sm text-gray-600">
@@ -66,12 +90,14 @@ export function Rodape() {
                   href="https://api.whatsapp.com/send/?phone=5516993108637&text&type=phone_number&app_absent=0"
                 >
                   <span className="flex items-center">
-                    <FaWhatsapp className="mr-1" />(16) 99310-8637
+                    <FaWhatsapp className="mr-1" />
+                    (16) 99310-8637
                   </span>
                 </a>
                 <a className="hover:opacity-75">
                   <span className="flex items-center">
-                    <FaRegAddressBook className="mr-1" />(16) 3720-5691
+                    <FaRegAddressBook className="mr-1" />
+                    (16) 3720-5691
                   </span>
                 </a>
                 <a className="hover:opacity-75"> </a>
@@ -88,8 +114,22 @@ export function Rodape() {
             <div>
               <p className="font-medium">Localização</p>
               <nav className="flex flex-col mt-4 space-y-2 text-sm text-gray-500">
-                <a className="hover:opacity-75"> Av. 7 de Setembro, Nº 650 Sala 01 e 02 </a>
-                <a className="hover:opacity-75"> Bairro São José 14401-278</a>
+                <a
+                  className="hover:opacity-75"
+                  href="https://maps.app.goo.gl/FejJqAM4TTUXhMLu6"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <span className="flex items-center underline">
+                    <FaLocationDot className="mr-1" />
+                    Clique aqui para ver a melhor rota
+                  </span>
+                </a>
+                <a className="hover:opacity-75">
+                  {" "}
+                  Av. 7 de Setembro, Nº | 650 Sala 01 e 02{" "}
+                </a>
+                <a className="hover:opacity-75"> Bairro São José | 14401-278</a>
                 <a className="hover:opacity-75"> Franca - SP </a>
               </nav>
             </div>
@@ -97,7 +137,7 @@ export function Rodape() {
         </div>
         <p className="mt-8 text-xs text-zinc-950">
           <a
-            className="hover:text-orange-600 text-zinc-950 text-ml font-bold underline"
+            className="hover:text-gray-600 text-zinc-950 text-ml font-bold underline"
             href="/"
           >
             © Clinica Rita Pacheco
