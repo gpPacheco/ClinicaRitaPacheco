a54101b3b8c159fc957e5b2206b3af4d226c9c5f
Gabriel Pacheco <gabrielfppacheco@gmail.com>
2024-09-19 17:15:42 -0300

    Ajuste bg collor sessão procedimentos e textos

--- src/app/home/components/homeSections/procedimentos.tsx
+++ src/app/home/components/homeSections/procedimentos.tsx
@@ -1,51 +1,30 @@
 "use client";
 import Image from "next/image";
-import { SetStateAction, useState } from "react";
 
 export function Procedimentos() {
-  const [selectedProcedure, setSelectedProcedure] = useState<string | null>(null);
-
-  const handleProcedureClick = (procedure: string) => {
-    setSelectedProcedure(procedure);
-  };
+  const procedures = [
+    { title: "Unhas encravadas", image: "/2.jpg" },
+    { title: "Podologia Infantil", image: "/2.jpg" },
+    { title: "Micose", image: "/2.jpg" },
+  ];
 
   return (
-    <div className="w-full px-4 mb-12 mt-12">
+    <div className="w-full px-4 bg-gradient-to-b from-[#f7f0ea] to-[#dbbeb0] shadow-md">
       <div className="text-xl text-center mb-4">Nossas especialidades</div>
       <div className="flex flex-wrap justify-center items-center gap-4">
-        <Image
-          className="w-60 h-40 object-cover rounded-lg shadow-md cursor-pointer transition-transform transform hover:scale-105 hover:shadow-lg"
-          src="/2.jpg"
-          alt="Procedimento 1"
-          width={240}
-          height={160}
-          onClick={() => handleProcedureClick("Unhas encravadas")}
-        />
-        <Image
-          className="w-60 h-40 object-cover rounded-lg shadow-md cursor-pointer transition-transform transform hover:scale-105 hover:shadow-lg"
-          src="/2.jpg"
-          alt="Procedimento 2"
-          width={240}
-          height={160}
-          onClick={() => handleProcedureClick("Podologia Infantil")}
-        />
-        <Image
-          className="w-60 h-40 object-cover rounded-lg shadow-md cursor-pointer transition-transform transform hover:scale-105 hover:shadow-lg"
-          src="/2.jpg"
-          alt="Procedimento 3"
-          width={240}
-          height={160}
-          onClick={() => handleProcedureClick("Micose")}
-        />
+        {procedures.map((procedure) => (
+          <div key={procedure.title} className="text-center">
+            <Image
+              className="w-60 h-40 object-cover rounded-lg shadow-md cursor-pointer transition-transform transform hover:scale-105 hover:shadow-lg"
+              src={procedure.image}
+              alt={procedure.title}
+              width={240}
+              height={160}
+            />
+            <h2 className="text-lg font-semibold mt-2">{procedure.title}</h2>
+          </div>
+        ))}
       </div>
-      {selectedProcedure && (
-        <div className="mt-4 text-center">
-          <h2 className="text-2xl font-semibold">{selectedProcedure}</h2>
-          <p className="mt-2 text-gray-600">
-            Aqui você pode adicionar mais informações sobre o procedimento selecionado.
-          </p>
-        </div>
-      )}
     </div>
   );
 }
