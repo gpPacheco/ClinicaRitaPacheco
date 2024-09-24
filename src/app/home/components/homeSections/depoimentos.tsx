cfe1b16f4d3d9ff41fa2a9946722835da98fd018
Gabriel Pacheco <gabrielfppacheco@gmail.com>
2024-09-24 16:38:51 -0300

    Alteração de tipos de depoimentos

--- src/app/home/components/homeSections/depoimentos.tsx
+++ src/app/home/components/homeSections/depoimentos.tsx
@@ -1,70 +1,50 @@
-"use client";
-
-import { useState, useRef } from "react";
-import { ChevronLeft, ChevronRight } from "lucide-react";
+import { useState } from "react";
 
 export function Depoimentos() {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const carrosselRef = useRef<HTMLDivElement>(null);
-  const depoimentos = [1, 2, 3, 4, 5]; // Lista de depoimentos
-  const totalItems = depoimentos.length;
-
-  const handleNext = () => {
-    if (currentIndex < totalItems - 1) {
-      setCurrentIndex((prevIndex) => prevIndex + 1);
-    }
-  };
-
-  const handlePrev = () => {
-    if (currentIndex > 0) {
-      setCurrentIndex((prevIndex) => prevIndex - 1);
-    }
-  };
+  const depoimentos = [
+    {
+      id: 1,
+      nome: "Hélio Rubens",
+      videoUrl: "https://www.youtube.com/embed/D1AOCszhNKs",
+    },
+    {
+      id: 2,
+      nome: "Lula do Basquete",
+      videoUrl: "https://www.youtube.com/embed/9DXSue6Q5MU",
+    },
+  ]; // Lista de depoimentos com URLs de vídeos e nomes
 
   return (
     <div className="flex flex-col items-center justify-center py-16 text-center bg-gradient-to-b from-[#dbbeb0] to-[#f7f0ea] shadow-md">
-      <p className="text-lg font-bold text-gray-900 opacity-50 hover:opacity-100 transition duration-300 ease-in-out">
+      <p className="text-lg font-bold text-gray-900">
         O que dizem nossos pacientes
       </p>
 
-      <div className="relative flex items-center justify-center w-full mt-8">
-        {/* Botão para ir ao card anterior */}
-        <button
-          onClick={handlePrev}
-          className=" left-0 z-10 bg-gray-200 p-2 rounded-full shadow-lg hover:bg-gray-300 transition"
-          disabled={currentIndex === 0}
-        >
-          <ChevronLeft size={24} />
-        </button>
-
-        <div className="overflow-hidden w-full max-w-4xl">
+      {/* Container para os depoimentos */}
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 w-full max-w-4xl mt-8">
+        {depoimentos.map((item) => (
           <div
-            ref={carrosselRef}
-            className="flex transition-transform duration-500 ease-in-out"
-            style={{
-              transform: `translateX(-${(currentIndex * 100) / 3}%)`, // Move o carrossel com base no índice
-            }}
+            key={item.id}
+            className=" shadow-md rounded-md p-4 w-full relative ml-2 mr-2" 
           >
-            {depoimentos.map((item, index) => (
-              <div
-                key={index}
-                className="bg-white border border-gray-200 shadow-md rounded-md p-4 w-[calc(100%/2.5)] mx-2 flex-shrink-0"
-              >
-                <h2 className="text-lg font-bold">Depoimento {item}</h2>
-                <p className="text-gray-600">Texto do depoimento {item}</p>
-              </div>
-            ))}
-          </div>
-        </div>
+            {/* Relação de Aspecto 16:9 (widescreen) */}
+            <div className="relative pb-[56.25%] overflow-hidden"> 
+              <iframe
+                className="absolute top-0 left-0 w-full h-full"
+                src={item.videoUrl}
+                title={`Depoimento ${item.nome}`}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            </div>
 
-        {/* Botão para ir ao próximo card */}
-        <button
-          onClick={handleNext}
-          className=" right-0 z-10 bg-gray-200 p-2 rounded-full shadow-lg hover:bg-gray-300 transition"
-          disabled={currentIndex === totalItems - 1}
-        >
-          <ChevronRight size={24} />
-        </button>
+            {/* Nome do Paciente abaixo do vídeo */}
+            <p className="text-sm font-medium text-gray-900 mt-2">
+              {item.nome}
+            </p>
+          </div>
+        ))}
       </div>
     </div>
   );
