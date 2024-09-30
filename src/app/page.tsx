64463d352200b2de1f72e5100fc97d851813b3ad
Gabriel Pacheco <gabrielfppacheco@gmail.com>
2024-09-30 09:37:23 -0300

    Correção de importação

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
-import { EmblaCarousel } from "@/app/home/components/carrossel/EmblaCarousel";
+import Carousel from "./home/components/newCarousel/carousel";
 import { FaixaContato } from "@/app/home/components/homeSections/faixaContato";
 import { Procedimentos } from "@/app/home/components/homeSections/procedimentos";
 import { Depoimentos } from "./home/components/homeSections/depoimentos";
 import { Avaliacoes } from "./home/components/homeSections/avaliacoes";
+import { EmblaCarousel } from "./home/components/carrossel";
+
 
 export default function Home() {
   return (
     <div className="w-screen h-full flex flex-col">
       <div className="min-h-screen">
         <EmblaCarousel />
+        {/* <Carousel /> */}
         <FaixaContato />
         <Procedimentos />
         <Depoimentos />
