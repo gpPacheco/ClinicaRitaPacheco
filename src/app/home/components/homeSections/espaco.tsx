38e4130e444fb36524de1b27afac5938872100e1
Gabriel Pacheco <gabrielfppacheco@gmail.com>
2024-10-06 12:30:46 -0300

    :style: Altereação do estilo de carrossel

--- src/app/home/components/homeSections/espaco.tsx
+++ src/app/home/components/homeSections/espaco.tsx
@@ -1,101 +1,76 @@
 'use client';
-import { useEffect, useState } from "react";
-import useEmblaCarousel from "embla-carousel-react";
-import Autoplay from "embla-carousel-autoplay";
-import { ChevronLeft, ChevronRight } from 'lucide-react';
-import { useRouter } from "next/navigation";
+import Image from "next/image";
+import { Carousel } from "primereact/carousel";
 
-export const Local = () => {
-  const imgs: { url: string; link: string }[] = [
-    { url: "/podologia_infantil/foto_a.jpg", link: "" },
-    { url: "/podologia_infantil/foto_b.jpg", link: "" },
-    { url: "/sobre/3.jpg", link: "" },
-    { url: "/sobre/4.jpg", link: "" },
+export function Local() {
+  type CarouselItem = {
+    description: string;
+    id: string;
+    image: string;
+  };
+
+  const carouselTemplate = (carousel: CarouselItem) => {
+    return (
+      <div className="carousel-item flex justify-center">
+        <div className="carousel-image">
+          <Image
+            src={carousel.image}
+            alt={carousel.description}
+            width={800}   
+            height={500} 
+            className="object-cover rounded-lg shadow-lg"
+          />
+        </div>
+        <div className="carousel-detail">
+          <p className="mt-4 text-center text-lg text-gray-700">
+            {carousel.description}
+          </p>
+        </div>
+      </div>
+    );
+  };
+
+  const carouselEspacoGeral = [
+    { id: "1", image: "/main-banner_a.jpg" },
+    { id: "2", image: "/main-banner_b.jpg" },
+    { id: "3", image: "/main-banner_c.jpg" },
+    { id: "4", image: "/main-banner_d.jpg" },
+    { id: "5", image: "/main-banner_e.jpg" },
   ];
 
-  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [
-    Autoplay({ delay: 3000 }),
-  ]);
-  const [selectedIndex, setSelectedIndex] = useState(0);
-
-  useEffect(() => {
-    if (emblaApi) {
-      emblaApi.on("select", () => setSelectedIndex(emblaApi.selectedScrollSnap()));
-    }
-  }, [emblaApi]);
-
-  const router = useRouter();
-
-  function navigate(link: string) {
-    if (link) {
-      router.push(link);
-    }
-  }
-
-  const goToPrevSlide = () => {
-    if (emblaApi) {
-      emblaApi.scrollPrev();
-    }
-  };
-
-  const goToNextSlide = () => {
-    if (emblaApi) {
-      emblaApi.scrollNext();
-    }
-  };
-
-  const scrollTo = (index: number) => {
-    if (emblaApi) {
-      emblaApi.scrollTo(index);
-    }
-  };
-
   return (
-    <div className="embla w-full shadow-md px-4 bg-gradient-to-b from-[#dbbeb0] via-[#f7f0ea] to-[#dbbeb0]" ref={emblaRef}>
-      <h2 className="text-2xl font-bold text-gray-900 mb-8 text-center">
-          Conheça nosso espaço
-        </h2>
-      <div className="embla__container">
-        {imgs.map((item, index) => (
-          <button
-            onClick={() => navigate(item.link)}
-            key={index}
-            className="embla__slide"
-            style={{ backgroundImage: `url(${item.url})` }}
-          ></button>
-        ))}
-      </div>
+    <div className="px-4 bg-gradient-to-b from-[#dbbeb0] via-[#f7f0ea] to-[#dbbeb0] flex flex-col items-center">
+      <h1 className="text-2xl font-light text-gray-800 sm:text-5xl lg:text-6xl text-center mb-10">
+        Conheça Nosso{" "}
+        <span className="font-light text-transparent bg-clip-text bg-gradient-to-r from-orange-500 to-orange-700">
+          Espaço
+        </span>
+      </h1>
 
-      {/* Botão anterior */}
-      <button
-        className="absolute bottom-0 left-0 z-[1] flex w-[15%] h-full items-center justify-center bg-transparent text-white transition-opacity duration-150 ease-in-out hover:text-black hover:opacity-90 focus:text-black focus:opacity-90"
-        type="button"
-        onClick={goToPrevSlide}
-      >
-        <ChevronLeft size={50} />
-      </button>
-
-      {/* Botão próximo */}
-      <button
-        className="absolute bottom-0 right-0 z-[1] flex w-[15%] h-full items-center justify-center bg-transparent text-white transition-opacity duration-150 ease-in-out hover:text-black hover:opacity-90 focus:text-black focus:opacity-90"
-        type="button"
-        onClick={goToNextSlide}
-      >
-        <ChevronRight size={50} />
-      </button>
-
-      {/* Bolinhas de Navegação */}
-      <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 z-[1] flex space-x-2">
-        {imgs.map((_, index) => (
-          <button
-            key={index}
-            className={`w-3 h-3 rounded-full transition-all ${
-              index === selectedIndex ? 'bg-white' : 'bg-gray-400'
-            }`}
-            onClick={() => scrollTo(index)}
-          />
-        ))}
-      </div>
+      {/* Seção do Espaço Geral */}
+      <section className="w-full max-w-[1200px]">
+        <Carousel
+          value={carouselEspacoGeral}
+          numVisible={1} 
+          numScroll={1}
+          circular
+          autoplayInterval={3000}
+          itemTemplate={carouselTemplate}
+          className="mx-auto" 
+          responsiveOptions={[
+            {
+              breakpoint: "1024px",
+              numVisible: 1,
+              numScroll: 1,
+            },
+            {
+              breakpoint: "768px",
+              numVisible: 1,
+              numScroll: 1,
+            },
+          ]}
+        />
+      </section>
     </div>
   );
-};
+}
