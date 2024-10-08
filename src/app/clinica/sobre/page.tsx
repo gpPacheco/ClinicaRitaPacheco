9109fbd985f347c328efb5765fea3727ca1e4917
Gabriel Pacheco <gabrielfppacheco@gmail.com>
2024-10-08 15:45:53 -0300

    style: Alteração de cor e tamanho de font título dos cards

--- src/app/clinica/sobre/page.tsx
+++ src/app/clinica/sobre/page.tsx
@@ -18,7 +18,7 @@ export default function Sobre() {
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 mb-16">
         <div className="bg-[#dbbeb0] p-6 rounded-lg shadow-md hover:shadow-lg transition duration-300 transform hover:scale-105 text-center">
           <FaHandHoldingHeart className="text-4xl text-zinc-600 mx-auto mb-4" />
-          <h2 className="text-2xl font-bold text-gray-800 mb-2">Missão</h2>
+          <h2 className="text-2xl font-light text-gray-800 mb-2">Missão</h2>
           <p className="text-gray-700">
             Oferecer cuidados podológicos de alta qualidade, priorizando a saúde
             e o bem-estar dos nossos pacientes.
@@ -27,7 +27,7 @@ export default function Sobre() {
 
         <div className="bg-[#dbbeb0] p-6 rounded-lg shadow-md hover:shadow-lg transition duration-300 transform hover:scale-105 text-center">
           <FaRegLightbulb className="text-4xl text-zinc-600 mx-auto mb-4" />
-          <h2 className="text-2xl font-bold text-gray-800 mb-2">Visão</h2>
+          <h2 className="text-2xl font-light text-gray-800 mb-2">Visão</h2>
           <p className="text-gray-700">
             Ser referência em podologia, promovendo saúde preventiva e
             tratamentos avançados para todas as idades.
@@ -36,7 +36,7 @@ export default function Sobre() {
 
         <div className="bg-[#dbbeb0] p-6 rounded-lg shadow-md hover:shadow-lg transition duration-300 transform hover:scale-105 text-center">
           <FaMedal className="text-4xl text-zinc-600 mx-auto mb-4" />
-          <h2 className="text-2xl font-bold text-gray-800 mb-2">Valores</h2>
+          <h2 className="text-2xl font-light text-gray-800 mb-2">Valores</h2>
           <p className="text-gray-700">
             Compromisso, ética, respeito e dedicação para oferecer o melhor
             atendimento aos nossos pacientes.
@@ -46,7 +46,7 @@ export default function Sobre() {
 
       {/* Seção de História da Clínica */}
       <div className="bg-[#dbbeb0] p-10 rounded-lg mb-10 shadow-md hover:shadow-lg transition duration-300 transform hover:scale-105 text-center max-w-4xl">
-        <h2 className="text-3xl font-bold text-gray-800 mb-6">Nossa História</h2>
+        <h2 className="text-3xl font-light text-gray-800 mb-6">Nossa História</h2>
         <p className="text-lg text-gray-700">
           Fundada em 2010 pela podóloga Rita Pacheco, nossa clínica vem se
           destacando pela excelência no atendimento e nos cuidados com os pés de
