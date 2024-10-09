9bfc817701f0fed0d4d99410b7d1a472b2da003a
Gabriel Pacheco <gabrielfppacheco@gmail.com>
2024-10-09 16:00:55 -0300

    feat: Adição de imagem, link de curso e alteração de texto da página

--- src/app/clinica/biosec/page.tsx
+++ src/app/clinica/biosec/page.tsx
@@ -6,28 +6,32 @@ export default function Biosec() {
   return (
     <div className="px-4 py-12 bg-gradient-to-b from-[#f7f0ea] to-[#dbbeb0] text-center">
       <h2 className="text-4xl font-light text-gray-800 sm:text-5xl lg:text-6xl text-center mb-10">
-        Biossegurança
+        Biossegurança e{" "}
+        <span className="font-light text-transparent bg-clip-text bg-gradient-to-r from-orange-500 to-orange-700">
+          Estereliação
+        </span>
       </h2>
-      
-      <section className="mb-8 text-center">
+
+      <div className="mb-8 text-center">
         <Image
-          src="/biosecurity-image.jpg"
+          src="/biosseguranca/1.png"
           alt="Biossegurança"
-          className="w-full h-64 object-cover rounded-lg mb-6"
-          width={500}
-          height={300}
+          className="mx-auto w-full sm:w-3/4 md:w-1/2 lg:w-1/3 h-auto object-contain rounded-lg mb-6"
+          width={800}
+          height={800}
         />
+
         <h3 className="text-2xl font-semibold text-gray-800 mb-4">
           O que é Biossegurança?
         </h3>
         <p className="text-lg text-gray-700 mb-4">
-          Biossegurança é um conjunto de práticas destinadas a proteger a
-          saúde das pessoas, animais e o meio ambiente. Em nossa clínica, a
+          Biossegurança é um conjunto de práticas destinadas a proteger a saúde
+          das pessoas, animais e o meio ambiente. Em nossa clínica, a
           biossegurança é uma prioridade para garantir o bem-estar de todos.
         </p>
-      </section>
+      </div>
 
-      <section className="mb-8">
+      <div className="mb-8">
         <h3 className="text-2xl font-semibold text-gray-800 mb-4">
           Nossas Práticas de Biossegurança
         </h3>
@@ -37,20 +41,20 @@ export default function Biosec() {
           <li>Treinamento contínuo da nossa equipe.</li>
           <li>Controle rigoroso de acesso às áreas sensíveis.</li>
         </ul>
-      </section>
+      </div>
 
-      <section className="mb-8">
+      <div className="mb-8">
         <h3 className="text-2xl font-semibold text-gray-800 mb-4">
           Por que a Biossegurança é Importante?
         </h3>
         <p className="text-lg text-gray-700">
-          A biossegurança é crucial para prevenir infecções e garantir a
-          saúde dos nossos pacientes e colaboradores. Com protocolos rigorosos,
+          A biossegurança é crucial para prevenir infecções e garantir a saúde
+          dos nossos pacientes e colaboradores. Com protocolos rigorosos,
           criamos um ambiente seguro e saudável.
         </p>
-      </section>
+      </div>
 
-      <section className="mb-8">
+      <div className="mb-8">
         <h3 className="text-2xl font-semibold text-gray-800 mb-4">
           Quer Saber Mais?
         </h3>
@@ -59,12 +63,13 @@ export default function Biosec() {
           biossegurança, confira nossos materiais educativos!
         </p>
         <a
-          href="/materiais-biosseguranca" // Altere para o link correto da sua página de materiais
+          href="https://chk.eduzz.com/1W3VPGX592"
           className="inline-block bg-orange-400 text-white px-6 py-2 rounded shadow-md hover:bg-orange-500 transition"
+          target="_blank"
         >
           Ver Materiais
         </a>
-      </section>
+      </div>
     </div>
   );
 }
