f33ef6bd68c6464ace065b66e2059c75890d3909
Gabriel Pacheco <gabrielfppacheco@gmail.com>
2024-10-10 17:40:11 -0300

    style: Ajuste no espaçamento do título da seção de procedimentos

--- src/app/home/components/homeSections/procedimentos.tsx
+++ src/app/home/components/homeSections/procedimentos.tsx
@@ -3,22 +3,43 @@ import { FaUserMd, FaChild, FaSyringe } from "react-icons/fa"; // Exemplos de í
 
 export function Procedimentos() {
   const procedures = [
-    { title: "Onicocriptose", icon: <FaUserMd />, description: "Tratamento eficaz para aliviar dores causadas por unhas encravadas." },
-    { title: "Podologia Infantil", icon: <FaChild />, description: "Cuidados especializados para a saúde dos pés das crianças." },
-    { title: "Onicomicose", icon: <FaSyringe />, description: "Tratamento da infecção fúngica das unhas, com resultados seguros." },
+    {
+      title: "Onicocriptose",
+      icon: <FaUserMd />,
+      description:
+        "Tratamento eficaz para aliviar dores causadas por unhas encravadas.",
+    },
+    {
+      title: "Podologia Infantil",
+      icon: <FaChild />,
+      description: "Cuidados especializados para a saúde dos pés das crianças.",
+    },
+    {
+      title: "Onicomicose",
+      icon: <FaSyringe />,
+      description:
+        "Tratamento da infecção fúngica das unhas, com resultados seguros.",
+    },
   ];
 
   return (
     <div className="w-full px-4 py-8 bg-gradient-to-b from-[#f7f0ea] to-[#dbbeb0] shadow-lg p-3">
       <div className="text-3xl font-light text-gray-800 text-center mb-5">
-        Confira nossas {" "}
-        <a href="/especialidades" className="font-light text-transparent bg-clip-text bg-gradient-to-r from-orange-500 to-orange-700">
+        Confira nossas{"  "}
+        <a
+          href="/especialidades"
+          className="font-light text-transparent bg-clip-text bg-gradient-to-r from-orange-500 to-orange-700"
+        >
           Especialidades
         </a>
       </div>
+
       <div className="flex flex-wrap justify-center items-center gap-6">
         {procedures.map((procedure) => (
-          <div key={procedure.title} className="bg-[#dbbeb0] w-64 p-6 rounded-lg shadow-md text-center transition-transform transform hover:scale-105 hover:shadow-lg">
+          <div
+            key={procedure.title}
+            className="bg-[#dbbeb0] w-64 p-6 rounded-lg shadow-md text-center transition-transform transform hover:scale-105 hover:shadow-lg"
+          >
             <div className="flex justify-center mb-4">
               <div className="text-4xl text-zinc-600">{procedure.icon}</div>
             </div>
