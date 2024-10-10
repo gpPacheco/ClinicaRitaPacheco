b46abf74dc6967773e987bcdab38bf86cce8b712
Gabriel Pacheco <gabrielfppacheco@gmail.com>
2024-10-10 17:36:21 -0300

    feat: Adicionar botão de agendamento e minimapa, alteração de textos e exlusão do formulário de contato

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -1,134 +1,85 @@
-'use client'
-import { useState } from 'react';
+"use client";
+import { FaixaContato } from "../components/button";
 
 export default function ContactForm() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
-  const [message, setMessage] = useState('');
-
-  const handleSubmit = async (event: { preventDefault: () => void; }) => {
-    event.preventDefault();
-
-    try {
-      // Send the form data to your server using a fetch request
-      const response = await fetch('/api/contact', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ name, email, phone, message }),
-      });
-
-      if (response.ok) {
-        // Handle successful submission, e.g., display a success message
-        console.log('Message sent successfully!');
-        // Reset the form fields
-        setName('');
-        setEmail('');
-        setPhone('');
-        setMessage('');
-      } else {
-        // Handle error, e.g., display an error message
-        console.error('Failed to send message!');
-      }
-    } catch (error) {
-      // Handle network errors, e.g., display an error message
-      console.error('Network error:', error);
-    }
-  };
-
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-4">Fale Conosco</h1>
-      <p className="mb-8">Envie suas dúvidas e sugestões</p>
-      <form onSubmit={handleSubmit} className="space-y-4">
-        <div>
-          <label htmlFor="name" className="block text-gray-700 font-bold mb-2">
-            Nome Completo
-          </label>
-          <input
-            type="text"
-            id="name"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
+    <div className="px-4 py-12 bg-gradient-to-b from-[#f7f0ea] to-[#dbbeb0]">
+      <h1 className="text-4xl font-light text-gray-800 sm:text-5xl lg:text-6xl text-center mb-10">
+        Fale Conosco
+      </h1>
+      <p className="mb-8 text-center text-lg text-gray-700">
+        Envie suas dúvidas e sugestões. Estamos prontos para ajudar!
+      </p>
+
+      <div className="grid lg:grid-cols-3 md:grid-cols-1 sm:grid-cols-1 text-center">
+        <div className="mt-8">
+          <h2 className="text-2xl text-gray-800 mb-4">Endereço</h2>
+          <p className="text-lg text-gray-700">
+            Av. 7 de Setembro, 650 | Sala 1
+          </p>
+          <p className="text-lg text-gray-700">Bairro São José | 14401-278</p>
+          <p className="text-lg text-gray-700">Franca/SP</p>
+          
         </div>
-        <div>
-          <label htmlFor="email" className="block text-gray-700 font-bold mb-2">
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
+
+        <div className="mt-8">
+          <h2 className="text-2xl text-gray-800 mb-4">
+            Horários de Atendimento
+          </h2>
+          <p className="text-lg text-gray-700">Segunda à Sexta-feira</p>
+          <p className="text-lg text-gray-700">08h00 - 11h30 | 13h30 - 18h00</p>
+          <p className="text-lg text-gray-700 mt-2">
+            Horário especial: (16) 99310-8637
+          </p>
         </div>
-        <div>
-          <label htmlFor="phone" className="block text-gray-700 font-bold mb-2">
-            Telefone
-          </label>
-          <input
-            type="tel"
-            id="phone"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)}
-          />
+
+        <div className="mt-8">
+          <h2 className="text-2xl text-gray-800 mb-4">
+            Contato
+          </h2>
+          <ul className="mt-4 space-y-2 text-lg">
+            <li>
+              <a
+                href="tel:+551637205691"
+                className="hover:underline"
+              >
+                <i className="fas fa-phone-alt"></i> (16) 3720-5691
+              </a>
+            </li>
+            <li>
+              <a
+                href="tel:+5516993108637"
+                className="hover:underline"
+              >
+                <i className="fas fa-mobile-alt"></i> (16) 99310-8637
+              </a>
+            </li>
+            <li>
+              <a
+                href="mailto:clinica@clinicaritapacheco.com.br"
+                className="hover:underline"
+              >
+                <i className="fas fa-envelope"></i>{" "}
+                clinica@clinicaritapacheco.com.br
+              </a>
+            </li>
+          </ul>
         </div>
-        <div>
-          <label htmlFor="message" className="block text-gray-700 font-bold mb-2">
-            Mensagem
-          </label>
-          <textarea
-            id="message"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            required
-          />
-        </div>
-        <button
-          type="submit"
-          className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus-shadow-outline"
-        >
-          Enviar Mensagem
-        </button>
-      </form>
-      <div className="mt-8">
-        <h2 className="text-xl font-bold mb-4">Clínica</h2>
-        <p>Av. 7 de Setembro, 650 | Sala 1</p>
-        <p>Bairro São José | 14401-278</p>
-        <p>Franca/SP</p>
-        <ul className="mt-4">
-          <li>
-            <a href="tel:+551637205691">
-              <i className="fas fa-phone-alt"></i> 16.3720.5691
-            </a>
-          </li>
-          <li>
-            <a href="tel:+5516993108637">
-              <i className="fas fa-mobile-alt"></i> 16-99310.8637
-            </a>
-          </li>
-          <li>
-            <a href="mailto:clinica@clinicaritapacheco.com.br">
-              <i className="fas fa-envelope"></i> clinica@clinicaritapacheco.com.br
-            </a>
-          </li>
-        </ul>
       </div>
-      <div className="mt-8">
-        <h2 className="text-xl font-bold mb-4">Horários</h2>
-        <p>Segunda à Sexta-feira</p>
-        <p>08h00-11h30</p>
-        <p>13h30-18h00</p>
-        <p>Horário especial: 16-99310.8637</p>
+      
+      <div className="mt-6">
+     <FaixaContato />
+      </div>
+
+      <div className="mt-12">
+        <h2 className="text-2xl text-gray-800 mb-4 text-center">Localização</h2>
+        <iframe
+          className="w-full h-80 rounded-lg shadow-lg"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3687.563340110967!2d-47.399299!3d-20.536321!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94b6f941d77f1b8b%3A0x5cb7286ab5cda5da!2sAv.%207%20de%20Setembro%2C%20650%20-%20S%C3%A3o%20Jos%C3%A9%2C%20Franca%20-%20SP%2C%2014401-278!5e0!3m2!1sen!2sbr!4v1696865609804!5m2!1sen!2sbr"
+          loading="lazy"
+          allowFullScreen
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
       </div>
     </div>
   );
