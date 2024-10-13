ac68bb373fe3c6d40a636c5e1ffb1fe289598f84
Gabriel Pacheco <gabrielfppacheco@gmail.com>
2024-10-13 11:31:38 -0300

    style: Alteração de BG de page not-found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function NotFound() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
+    <div className="flex items-center justify-center h-screen bg-gradient-to-b from-[#f7f0ea] to-[#dbbeb0]">
       <div className="text-center">
         <h1 className="text-4xl font-bold text-gray-800 mb-4">Página não encontrada</h1>
         <p className="text-lg text-gray-600">Desculpe, a página que você está procurando não existe.</p>
