fea2ca1d92e12584f12e1b6cac836cf72c604fad
Gabriel Pacheco <gabrielfppacheco@gmail.com>
2024-10-22 16:23:38 -0300

    refactor: Ajuste no arredondamento do botão e na cor de fundo da pg

--- src/app/produtos/page.tsx
+++ src/app/produtos/page.tsx
@@ -7,7 +7,7 @@ export default function Produtos() {
   const router = useRouter();
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-[#f7f0ea] text-center">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-[#f7f0ea] to-[#dbbeb0] text-center">
       <h1 className="text-4xl font-semibold text-[#7b4f38] mb-4">
         Página em construção 🚧
       </h1>
@@ -16,7 +16,7 @@ export default function Produtos() {
       </p>
       <button
         onClick={() => router.push('/')}
-        className="bg-[#F2784B] hover:bg-orange-500 text-white px-6 py-3 rounded-full shadow-md transition-transform transform hover:scale-105"
+        className="bg-[#F2784B] hover:bg-orange-500 text-white px-6 py-3 rounded-md shadow-md transition-transform transform hover:scale-105"
       >
         Clique aqui para voltar para a página inicial
       </button>
