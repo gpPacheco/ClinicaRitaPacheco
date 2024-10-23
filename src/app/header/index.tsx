2772a34e25ef3c2c5eb87e68670fd632d0a61b46
Gabriel Pacheco <gabrielfppacheco@gmail.com>
2024-10-22 23:00:01 -0300

    Correção de import menuItem e adição de import do botão de redes sociais

--- src/app/header/index.tsx
+++ src/app/header/index.tsx
@@ -5,9 +5,10 @@ import Image from "next/image";
 import Link from "next/link";
 import { Fragment, useEffect, useRef, useState } from "react";
 import { FaAngleDown, FaShareAlt, FaWhatsapp, FaInstagram, FaFacebook, FaLinkedin, FaTiktok, FaBookOpen, FaBriefcaseMedical, FaClinicMedical, FaHome, FaPhone, FaShoppingCart, FaUserMd } from "react-icons/fa";
-import BurgerButton from './burgerButton'; // Importar o BurgerButton
-import MenuItem from './MenuItem'; // Importar o MenuItem
-import DropdownItem from './dropdownItem'; // Importar o DropdownItem
+import BurgerButton from './burgerButton';
+import MenuItem from './menuItem';
+import DropdownItem from './dropdownItem';
+import SocialMenu from "./socialMenu";
 
 const navigation = [
   { name: "Home", href: "/", current: true, icon: <FaHome /> },
@@ -142,112 +143,8 @@ export function Header() {
               </div>
 
               {/* Redes sociais button */}
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6">
-                <Menu as="div" className="relative ml-3">
-                  <div>
-                    <Menu.Button className="relative flex rounded-full px-1.5 py-1.5 text-gray-900 hover:bg-gray-600 hover:text-white focus:outline-none">
-                      <FaShareAlt className="h-5 w-5" />
-                    </Menu.Button>
-                  </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="absolute right-0 z-[102] mt-2 w-48 p-4 origin-top-right rounded-md transition-transform transform duration-300 ease-in-out bg-[#f7f0ea] py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="https://api.whatsapp.com/send/?phone=5516993108637&text&type=phone_number&app_absent=0"
-                            className={classNames(
-                              active ? "bg-gray-600 hover:text-white rounded-md" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                            target="_blank"
-                            rel="noreferrer"
-                          >
-                            <span className="flex items-center">
-                              <FaWhatsapp className="mr-2" /> WhatsApp
-                            </span>
-                          </a>
-                        )}
-                      </Menu.Item>
-                      {/* Outros itens de menu social... */}
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="https://www.instagram.com/ritafpacheco/"
-                            className={classNames(
-                              active ? "bg-gray-600 hover:text-white rounded-md" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                            target="_blank"
-                            rel="noreferrer"
-                          >
-                            <span className="flex items-center">
-                              <FaInstagram className="mr-2" /> Instagram
-                            </span>
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="https://www.facebook.com/ritapachecopodologa"
-                            className={classNames(
-                              active ? "bg-gray-600 hover:text-white rounded-md" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                            target="_blank"
-                            rel="noreferrer"
-                          >
-                            <span className="flex items-center">
-                              <FaFacebook className="mr-2" /> Facebook
-                            </span>
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="https://www.linkedin.com/company/clinica-rita-pacheco/"
-                            className={classNames(
-                              active ? "bg-gray-600 hover:text-white rounded-md" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                            target="_blank"
-                            rel="noreferrer"
-                          >
-                            <span className="flex items-center">
-                              <FaLinkedin className="mr-2" /> LinkedIn
-                            </span>
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="https://www.tiktok.com/@ritapacheco"
-                            className={classNames(
-                              active ? "bg-gray-600 hover:text-white rounded-md" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}
-                            target="_blank"
-                            rel="noreferrer"
-                          >
-                            <span className="flex items-center">
-                              <FaTiktok className="mr-2" /> TikTok
-                            </span>
-                          </a>
-                        )}
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
+              <div>
+                <SocialMenu />
               </div>
             </div>
           </div>
