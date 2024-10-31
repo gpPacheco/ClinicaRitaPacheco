922b07bca1fc20b536980b5cb6d96b9774fe8dd3
Gabriel Pacheco <gabrielfppacheco@gmail.com>
2024-10-31 09:48:00 -0300

    Configuração de meta tags para SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,8 +19,35 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <head>
+        <meta
+          name="description"
+          content="Podóloga em Franca, especializada em cuidados dos pés para todas as idades. Encontre serviços de podologia, podologia esportiva, geriátrica e infantil."
+        />
+        <meta
+          name="keywords"
+          content="podóloga, podologia em Franca, podóloga em franca, podologia perto de mim, , podóloga perto de mim, unha encravada, micose, cuidados dos pés, podologia esportiva, podologia geriátrica, podologia infantil, Franca, São Paulo"
+        />
+        <meta name="robots" content="index, follow" />
+
+        <meta
+          property="og:title"
+          content="Podóloga em Franca - Cuidados Completo para Seus Pés"
+        />
+        <meta
+          property="og:description"
+          content="Encontre uma podóloga em Franca especializada em podologia esportiva, geriátrica e infantil. Cuidamos dos seus pés com carinho e profissionalismo."
+        />
+        <meta property="og:image" content="/logo-site.png" />
+        <meta property="og:image:alt" content="Logo da podóloga em Franca" />
+        <meta property="og:url" content="https://clinica-rita-pacheco.vercel.app" />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="pt_BR" />
+        <meta name="author" content="Rita Pacheco" />
+        <meta property="og:site_name" content="Podóloga em Franca" />
+
         <link rel="icon" href="/logo-site.png" />
       </head>
+
       <body className={`${poppins.className} overflow-x-hidden flex flex-col`}>
         <Header />
         <main className="w-screen h-full pt-20">{children}</main>
