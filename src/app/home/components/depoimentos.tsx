9dccc566226e5249bd71175ff6a9940b301ea9cc
gpPacheco <gabrielfppacheco@gmail.com>
2025-05-14 20:56:04 -0300

    Add lodaing lazy

--- src/app/home/components/depoimentos.tsx
+++ src/app/home/components/depoimentos.tsx
@@ -25,11 +25,10 @@ export function Depoimentos() {
               className="relative rounded-md overflow-hidden shadow-md"
             >
               <div className="pt-[56.25%]">
-                <iframe
+                <iframe loading="lazy"
                   className="absolute top-0 left-0 w-full h-full"
                   src={depoimento.videoUrl}
                   title={`Depoimento ${depoimento.id}`}
-                  frameBorder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                   allowFullScreen
                 ></iframe>
